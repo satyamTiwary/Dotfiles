526fc08a72c7969be9e4543f0ffde3e563a51d6b
Satyam tiwary <satyamtiwary@vvbiotech.com>
2020-07-11 18:48:01 +0000

    minor

--- setup.sh
+++ setup.sh
@@ -1,13 +1,19 @@
-echo "Copying .bash_aliases to the home directory."
+#!/bin/bash
+
+echo 'Copying .bash_aliases to the home directory.'
 cp ./.bash_aliases ~/
-echo "	-> Done.
-"
-echo "Copying .gitconfig to the home directory."
+echo '	-> Done.'
+echo 'Copying .gitconfig to the home directory.'
 cp ./.gitconfig ~/
-echo "	-> Done
-"
+echo '	-> Done'
 
 source ~/.bash_aliases
-echo "New aliases activated."
+echo 'New aliases activated.'
+
+# Environment Variables
+echo 'Setting environment variables'
+export NAME='SatyamTiwary'
+export EMAIL='satyamtiwary@vvbiotech.com'
+export GITHUB='https//github.com/$NAME/'
 
 cd ~
