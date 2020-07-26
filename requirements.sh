d2e68e8552331566d24eb5cc173dceba622a341f
Satyam tiwary <satyamtiwary@vvbiotech.com>
2020-07-26 17:48:58 +0530

    requirement script updates with installation commands for various formatters

--- requirements.sh
+++ requirements.sh
@@ -3,3 +3,18 @@
 # These dotfiles include various componenets with executable and folder structure dependencies
 # Over time, these can be presented here either as notes or an executable shell file
 
+
+# Vim-autoformat
+python3 -m pip install pynvim
+  # autopep8
+  pip3 install autopep8
+  # ESlint
+  npm install -g eslint
+  # html,css,js: Js-beautify
+  npm install -g js-beautify
+  # markdown
+  npm install -g remark-cli
+  # cmake
+  pip3 install cmake_format
+
+
