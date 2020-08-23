b14266a1316d0644114fda3adec712352b9c5501
Satyam tiwary <satyamtiwary@vvbiotech.com>
2020-08-23 23:00:43 +0530

    requirements script restructured

--- scripts/requirements.sh
+++ scripts/requirements.sh
@@ -3,15 +3,14 @@
 # These dotfiles include various componenets with executable and folder structure dependencies
 # Over time, these can be presented here either as notes or an executable shell file
 
-case $1  in
-  -c|--cli)
+function cli(){
     echo 
     echo "cli mode installations"
     echo 
-    sudo apt install vim tmux npm python3 exuberant-ctags tmuxinator net-tools texlive-binaries tree ipython3 tree poppler-utils texlive-latex-extra linux-tools-generic ghc hugs latexmk llvm clang lldb cabal-install 
+    sudo apt -y install vim tmux npm python3 exuberant-ctags tmuxinator net-tools texlive-binaries tree ipython3 tree poppler-utils texlive-latex-extra linux-tools-generic ghc hugs latexmk llvm clang lldb cabal-install 
     cabal update
-    sudo apt-get install texlive-latex-base texlive-latex-extra
-    pip3 install pynvim autopep8 cmake_format
+    sudo apt-get -y install texlive-latex-base texlive-latex-extra asciidoctor
+    pip3 -y install pynvim autopep8 cmake_format
     sudo npm install -g eslint js-beautify remark-cli scmindent livedown
 
     # Repo tools
@@ -33,33 +32,38 @@ https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
     echo 
     echo "---"
     echo
-    ;;
+}
 
-  -g|--graphical)
+function gui(){
     echo
     echo "graphical mode installations"
     echo 
-    sudo apt-get install ghc libghc-regex-compat-dev libghc-syb-dev libghc-old-time-dev libghc-split-dev tcl-dev autoconf gperf flex bison iverilog verilator gtkwave 
-    sudo snap install --classic code typora
+    sudo apt-get -y install ghc libghc-regex-compat-dev libghc-syb-dev libghc-old-time-dev libghc-split-dev tcl-dev autoconf gperf flex bison iverilog verilator gtkwave 
+    sudo snap -y install --classic code typora
     echo
     echo "---"
     echo 
-    ;;
+}
 
-  -h|--help)
+function help(){
     echo 
     echo "./requirements.sh [-i|-g|-h]"
     echo 
-    echo " -c | --cli         : Install Base Set of Packages associated with a command line system"
-    echo " -g | --graphical   : Install Packages associated with a graphical system"
-    echo " -h | --help        : Print help text associated with this script"
+    echo " -c | --cli    : Install Base Set of Packages associated with a command line system"
+    echo " -g | --gui    : Install Packages associated with a graphical system"
+    echo " -h | --help   : Print help text associated with this script"
     echo
     echo "---"
     echo
-    ;;
+  }
 
-  *)
-    echo "default"
-    ;;
+
+## 
+## string -> (void -> void)
+case $1  in
+  -c|--cli) cli ;;
+  -g|--gui) cli; gui ;;
+  -h|--help) help ;;
+  *) help ;;
 esac 
 
